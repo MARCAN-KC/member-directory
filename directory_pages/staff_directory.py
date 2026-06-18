cf63f2eb7c0c3ebb0835f75eba44bc862f3ad8d0
Joseph Cho <ujocho0913@gmail.com>
2026-06-18 15:55:33 -0500

    adjusted for marcan directory

--- directory_pages/staff_directory.py
+++ directory_pages/staff_directory.py
@@ -9,9 +9,17 @@ Updated: November 10, 2025
 import streamlit as st
 from pathlib import Path 
 import pandas as pd
+import re
 
-from connect_data import STAFF_DIRECTORY # Load data
+
+from read_data import MEMBERS # Load data
 from photo import load_photo
+
+
+MEMBERS['City'] = MEMBERS['agency_address'].str.split(',').str[1].str.strip()
+MEMBERS['State'] = MEMBERS['agency_address'].str.split(',').str[2].str.strip()
+
+st.dataframe(MEMBERS)
 
 # st.title("Staff Directory")
 # TODO - directory pagination? 
@@ -25,20 +33,17 @@ from photo import load_photo
 
 # --- Initialize session state --- 
 
-if "staffview_selected_position" not in st.session_state:
-    st.session_state["staffview_selected_position"] = "All"
+if "city" not in st.session_state:
+    st.session_state["city"] = "All"
 
-if "staffview_selected_unit" not in st.session_state:
-    st.session_state["staffview_selected_unit"] = "All"
+if "state" not in st.session_state:
+    st.session_state["state"] = "All"
 
-if "staffview_selected_location" not in st.session_state:
-    st.session_state["staffview_selected_location"] = "All"
+if "exec_board" not in st.session_state: # exec board ONLY? 
+    st.session_state["exec_board"] = False
 
-if "staffview_selected_month" not in st.session_state:
-    st.session_state["staffview_selected_month"] = "All"
-
-if "staffview_searched_text" not in st.session_state:
-    st.session_state["staffview_searched_text"] = ""
+if "member_name" not in st.session_state:
+    st.session_state["member_name"] = ""
 
 
 # --- Define callback functions --- 
@@ -46,148 +51,88 @@ if "staffview_searched_text" not in st.session_state:
 # Define update_df() function
 def update_df():
 
-    filtered_df = STAFF_DIRECTORY.copy()
+    filtered_df = MEMBERS.copy()
 
-    if st.session_state["staffview_selected_position"] != 'All':
-        filtered_df = filtered_df[filtered_df['Position']==st.session_state["staffview_selected_position"]].reset_index(drop=True)
-    if st.session_state["staffview_selected_unit"] != 'All':
-        filtered_df = filtered_df[filtered_df['Assigned Unit'].apply(lambda x: st.session_state["staffview_selected_unit"] in x)].reset_index(drop=True)
-    if st.session_state["staffview_selected_location"] != 'All': 
-        filtered_df = filtered_df[filtered_df['Office Location']==st.session_state["staffview_selected_location"]].reset_index(drop=True)
-    if st.session_state["staffview_selected_month"] != 'All':
-        filtered_df = filtered_df[filtered_df['DOB Month']==int(st.session_state["staffview_selected_month"])].reset_index(drop=True)
-    if st.session_state["staffview_searched_text"]: 
+    if st.session_state["city"] != 'All':
+        filtered_df = filtered_df[filtered_df['agency_address'].str.contains(st.session_state["city"], case=False, na=False)].reset_index(drop=True)
+    if st.session_state["state"] != 'All':
+        filtered_df = filtered_df[filtered_df['State']==st.session_state["state"]].reset_index(drop=True)
+    if st.session_state["exec_board"]:
+        filtered_df = filtered_df[filtered_df['exec_board']].reset_index(drop=True)
+    if st.session_state["member_name"]:
         # Update search function to split entered text into unique words, and check if any words appear in specified cols
 
-        searched_text = st.session_state["staffview_searched_text"].strip().lower()
+        searched_text = st.session_state["member_name"].strip().lower()
         words = list({w for w in searched_text.split() if w}) # split search text into unique words
-        search_cols = ["Full Name", "First Name", "Middle Name", "Last Name", "Suffix", "Preferred Name"]
+        search_cols = ["full_name", "agency_name", "job_title", "tools"]
         combined = filtered_df[search_cols].astype(str).agg(" ".join, axis=1).str.lower() # combine searchable columns into a single lowercase string per row
         mask = combined.apply(lambda text: any(word in text for word in words)) # match if ANY word appears in the combined text
 
         filtered_df = filtered_df[mask].reset_index(drop=True)
 
-    st.session_state["staffview_filtered_df"] = filtered_df.reset_index(drop=True)
+    st.session_state["df"] = filtered_df.reset_index(drop=True)
 
 # Reset filters button
 def reset_filters():
-    st.session_state["staffview_selected_position"] = "All"
-    st.session_state["staffview_selected_unit"] = "All"
-    st.session_state["staffview_selected_location"] = "All"
-    st.session_state["staffview_selected_month"] = "All"
-    st.session_state["staffview_searched_text"] = ""
+    st.session_state["city"] = "All"
+    st.session_state["state"] = "All"
+    st.session_state["exec_board"] = False
+    st.session_state["member_name"] = ""
     # filtered_df = STAFF_DIRECTORY.copy()
     # st.session_state["staffview_filtered_df"] = filtered_df
-    st.session_state["staffview_filtered_df"] = STAFF_DIRECTORY
+    st.session_state["df"] = MEMBERS.copy()
 
 
 # --- Sidebar Filter functions ---
 
 with st.sidebar:
     # Select options: position / unit / location / birthday month 
-    st.title("Jackson County Prosecuting Attorney's Office")
-    st.write("***Internal Staff Directory***")
+    st.title("Mid-America Regional Crime Analysis Network (MARCAN)")
+    st.write("***Member Directory***")
     st.divider()
-    st.write("Please use the directory to view information on all attorneys and support staff actively employed in the Jackson County Prosecuting Attorney's Office.")
+    st.write("Please use the directory to view information on active MARCAN members!")
     st.divider()
+
+    # Filter by Exec Board
+    st.toggle(
+        ":blue-badge[⭐️ **Executive Board:**]",
+        key="exec_board",
+        help="Filter directory to only members of the Executive Board",
+        # width="stretch",
+    )
     
-    # Filter by job position (position_enum): 
-    positions_dict = { 
-        'All': 'All Job Positions', 
-        'Exec': 'Executive Staff', 
-        'CTA': 'Chief Trial Attorneys', 
-        'TTL': 'Trial Team Leaders', 
-        'APA': 'APAs',
-        'I': 'Investigators',
-        'VA': 'Victim Advocates',
-        'LA': 'Legal Assistants',
-        'SS': 'Support Staff',
-        'INTERN': 'Interns',
-        'PET': '🐾 Paw-secuting Attorneys'
+    # Filter by City
+    # cities_dict = {k: k for k in MEMBERS['City'].dropna().unique()}
+    cities_dict = {
+        f"{row['City']}, {row['State']}": f"{row['City']}, {row['State']}"
+        for _, row in MEMBERS[['City', 'State']].dropna().drop_duplicates().iterrows()
     }
-    position_options = st.selectbox(
-        label= ":green-badge[**Filter by Position:**]", # "Filter by Position:",
-        options=positions_dict.keys(), # ('All', 'Exec', 'CTA', 'TTL', 'APA', 'I', 'VA', 'LA', 'SS')
+    cities_options = st.selectbox(
+        label= ":blue-badge[**Filter by City:**]",
+        options=cities_dict.keys(),
         index=0, # All
-        format_func=lambda x: positions_dict[x],
-        key='staffview_selected_position',
-        placeholder="Select job position",
+        format_func=lambda x: cities_dict[x],
+        key='city',
+        placeholder="Select city",
         on_change=update_df,
     )
 
-    # Filter by assigned unit (unit_enum): 
-    units_dict = { 
-        'All': 'All Office Units',
-        'Exec': 'Executive Staff',
-        'GCU': 'GCU, General Crimes',
-        'SVU': 'SVU, Special Victims',
-        'VCU': 'VCU, Violent Crimes',
-        'CSU': 'CSU, Crime Strategies',
-        'COMBAT': 'COMBAT',
-        'Drug': 'Drug Court',
-        'FSD': 'Family Support',
-        'WARRANT': 'Warrant Desk'
-    }
-    unit_options = st.selectbox(
-        label=":blue-badge[**Filter by Assigned Unit:**]", # "Filter by Assigned Unit:",
-        options=units_dict.keys(), # ('Exec', 'GCU', 'SVU', 'VCU', 'CSU', 'COMBAT', 'Drug', 'FSD')
+    # Filter by State
+    states_dict = {k: k for k in MEMBERS['State'].dropna().unique()}
+    states_options = st.selectbox(
+        label= ":blue-badge[**Filter by State:**]",
+        options=states_dict.keys(),
         index=0, # All
-        format_func=lambda x: units_dict[x],
-        key='staffview_selected_unit',
-        placeholder="Select unit",
-        on_change=update_df,
-    )
-
-    # Filter by office location (location_enum): 
-    locations_dict = {
-        'All': 'All Office Locations',
-        'Dt-11': 'Downtown, 11th',
-        'Dt-10': 'Downtown, 10th',
-        'Dt-9': 'Downtown, 9th',
-        'Dt-7M': 'Downtown, 7M',
-        'Indy': 'East Jack, Independence',
-        'FSD': 'Family Support'
-    }
-    location_options = st.selectbox(
-        label=":orange-badge[**Filter by Office Location:**]", # "Filter by Office Location:",
-        options=locations_dict.keys(), # ('Dt-11', 'Dt-10', 'Dt-9', 'Dt-7M', 'Indy', 'FSD')
-        index=0, # All
-        format_func=lambda x: locations_dict[x],
-        key='staffview_selected_location',
-        placeholder="Select office location",
-        on_change=update_df,
-    )
-
-    # Filter by birthday month:
-    months_dict = {
-        'All': 'All Months',
-        '1': 'Jan',
-        '2': 'Feb',
-        '3': 'Mar',
-        '4': 'Apr',
-        '5': 'May',
-        '6': 'Jun',
-        '7': 'Jul',
-        '8': 'Aug',
-        '9': 'Sep',
-        '10': 'Oct',
-        '11': 'Nov',
-        '12': 'Dec'
-    }
-    month_options = st.selectbox(
-        label=":violet-badge[**Filter by Birthday Month:**]", # "Filter by Birthday Month:",
-        options=months_dict.keys(),
-        index=0, # All
-        format_func=lambda x: months_dict[x],
-        key='staffview_selected_month',
-        placeholder="Select birthday month",
+        format_func=lambda x: states_dict[x],
+        key='state',
+        placeholder="Select state",
         on_change=update_df,
     )
 
     # Reset filters 
     refresh = st.button(
         label="Reset Filters",
-        key="staffview_filter_reset",
+        key="reset",
         on_click=reset_filters,
         type="secondary",
         icon="🔄",
@@ -218,6 +163,13 @@ def reformat_phone_num(phone_num):
     else:
         return phone_num
 
+def to_bullet_list(text): # Separate by , ; \n \t (but not /)
+    if not text:
+        return ""
+    items = re.split(r'[,;\n\t]+', str(text))
+    items = [item.strip() for item in items if item.strip()]
+    return "\n" + "\n".join(f"- {item}" for item in items)
+
 def display_employee(row):
 
     with st.container():
@@ -227,107 +179,51 @@ def display_employee(row):
         with col1:
             
             # Headshot Photo (if None, JCPAO logo)
-            jcpao_logo = Path("assets/logo/jcpao_logo_500x500.png")
-            if row['PhotoID'] is None:
-                st.image(jcpao_logo, width=400)
+            marcan_logo = Path("assets/logo/marcan_logo.png")
+            if not row['photo']:
+                st.image(marcan_logo, width=400)
             else:
-                headshot_path = "JCPAO_headshots/"+row['PhotoID'].strip()
+                # headshot_path = "headshots/"+row['work_email'].strip() # work_email to replace PhotoID
+                headshot_path = row['work_email'].strip() # work_email to replace PhotoID
                 staff_headshot = load_photo(headshot_path)
                 st.image(staff_headshot, width=400)
+                # st.write(headshot_path)
             
         with col2:
-
-            # Employee Name
-            if row['Preferred Name']: # If preferred name exists
-                st.header(f"{row['Preferred Name'].strip()} {row['Last Name'].strip()}")
-            else:
-                st.header(f"{row['First Name'].strip()} {row['Last Name'].strip()}")
-
-            # Job Title
-            st.subheader(f"{row['Job Title']}")
-
-            # Position BADGE
-            positions_badge = {
-                'Exec': 'Exec Staff', 
-                'CTA': 'CTA', 
-                'TTL': 'TTL', 
-                'APA': 'APA',
-                'I': 'Investigator',
-                'VA': 'Victim Advocate',
-                'LA': 'Legal Assistant',
-                'SS': 'Support Staff',
-                'INTERN': 'Intern',
-                'PET': 'Paw-secuting Attorney 🐾'
-            }
-            # position_badge = st.badge(f"**Position:** {positions_badge.get(row['Position'], row['Position'])}", icon=None, color="green")
-
-            # Assigned Unit (unit_enum[]) BADGE
-            units_badge = {
-                'Exec': 'Exec Staff',
-                'GCU': 'GCU',
-                'SVU': 'SVU',
-                'VCU': 'VCU',
-                'CSU': 'CSU',
-                'COMBAT': 'COMBAT',
-                'Drug': 'Drug Court',
-                'FSD': 'Family Support',
-                'WARRANT': 'Warrant Desk'
-            } # units_badge.get(row['Assigned Unit'], row['Assigned Unit'])
-            # unit_badge = st.badge(f"**{' / '.join(units_badge.get(item, item) for item in row['Assigned Unit'])}**", icon=None, color="blue")
-
-            if row['Assigned Unit']:
-                unit = ' / '.join(row['Assigned Unit'])
-            else:
-                unit = "**N/A**" # ':red[???]'
-
-            # Office Location BADGE
-            locations_badge = {
-                'Dt-11': 'Downtown, 11th',
-                'Dt-10': 'Downtown, 10th',
-                'Dt-9': 'Downtown, 9th',
-                'Dt-7M': 'Downtown, 7M',
-                'Indy': 'Eastern Jack, Indy',
-                'FSD': 'Downtown, FSD'
-            }
-            # location_badge = st.badge(f"**Office Location:** {locations_badge.get(row['Office Location'], row['Office Location'])}", icon="🏢", color="orange")
-
-            # DOB Month / DOB Day BADGE 
-            if not pd.isna(row['DOB Month']):
-                dob_month = int(row['DOB Month'])
-            else:
-                dob_month = "N"
-            if not pd.isna(row['DOB Day']):
-                dob_day = int(row['DOB Day'])
-            else:
-                dob_day = "A"
-            # dob_badge = st.badge(f"**Birthday:** {row['DOB Month']}/{row['DOB Day']}", icon="🎉", color="violet")
-
-            # Display BADGE thread 
-            st.markdown( # position_badge, unit_badge, location_badge, dob_badge
-                f":green-badge[**{positions_badge.get(row['Position'], row['Position'])}**]"
-                # f":blue-badge[**{' / '.join(units_badge.get(item, item) for item in row['Assigned Unit'])}**]"
-                f":blue-badge[**{unit}**]"
-                f":orange-badge[🏢 **{locations_badge.get(row['Office Location'], row['Office Location'])}**]"
-                f":violet-badge[🎉 **{dob_month}/{dob_day}**]"
-            )
-
-            # Work Phone Number
-            work_phone = reformat_phone_num(row['Work Phone #'])
-            if str(row['Work Phone #']).startswith("816881"):
-                st.write(f"**Work Phone:** {work_phone} (ext. {str(row['Work Phone #'])[-4:]})")
-            else:
-                st.write(f"**Work Phone:** {work_phone}")
+            exec_badge = f":blue-badge[⭐️ ***{row['exec_position']}***]" if row['exec_board'] else ""
             
-            # Work Email Address
-            st.write(f"**Work Email:** {row['Work Email Address']}")
+            st.markdown(f"""
+                # {row['full_name']}     
+                {exec_badge}    
 
-            # Personal Phone Number
-            personal_phone = reformat_phone_num(row['Personal Phone #'])
-            st.write(f"**Personal Phone:** {personal_phone}")
+                **Agency:** {row['agency_name']}    
+                **Job Title:** {row['job_title']}   
+                **Work Email:** {row['work_email']}    
+                **Work Phone:** {reformat_phone_num(row['work_phone'])}    
+            """)
 
-            # Personal Email Address - exclude, for now 
-            # st.write(f"**Personal Email:** {row['Personal Email Address']}")
-            
+            bio = row['bio']
+            add_exp_cleaned = row['add_exp'].replace('\n', ', ') if pd.notna(row['add_exp']) else None
+            additional_experience = f"**Additional Experience:**\n{to_bullet_list(add_exp_cleaned)}" if add_exp_cleaned else ""
+            tools = row['tools']
+
+            with st.expander(f"**More about {row['full_name'].split()[0]}:**", expanded=False, width="stretch", icon=":material/person_book:"):
+                st.markdown(f"""
+                    **Biography:**      
+                    {bio}      
+                """)
+
+                st.markdown(f"""
+                    {additional_experience}
+                """)
+
+            with st.expander(f"**Software Tools:**", expanded=False, width="stretch", icon=":material/build:"):
+                st.markdown(f"""
+                    **Tools:**       
+                    {to_bullet_list(tools)}      
+                """)
+
+
         st.divider()
 
 # --- Display INTERNAL Directory ---
@@ -335,30 +231,34 @@ def display_employee(row):
 # filtered_df = emp_view.copy()
 # filtered_df.reset_index(drop=True, inplace=True)
 
-df = st.session_state.get("staffview_filtered_df", STAFF_DIRECTORY)
+df = st.session_state.get("df", MEMBERS.copy())
 
 # Internal Directory title 
-st.markdown("<h1 style='text-align: center; color: black;'>Internal Staff Directory</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>MARCAN Member Directory</h1>", unsafe_allow_html=True)
 st.divider()
 
 # Text Search feature 
 searched_text = st.text_input(
     "Search employee name:",
-    key="staffview_searched_text"
+    key="member_name"
 )
 
 text_search = st.button(
     "Search",
     icon="🔎",
     on_click=update_df,
-    key="staffview_text_search",
+    key="search_button",
 )
 
 st.divider()
 
 if df.empty:
-    st.info("No active JCPAO staff found given the selected category filters.", icon="⚠️")
+    st.info("No active MARCAN members found given the selected category filters.", icon="⚠️")
 else:
     for i, row in df.iterrows():
         display_employee(row)
+
+
+
+
 
